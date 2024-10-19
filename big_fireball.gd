5d342107cb5f490a44f304c4b3b1652e57043b42
AaronRodden <aaron.rodden21@gmail.com>
2024-10-19 13:42:04 -0700

    cleaing up some balance changes to prep for merge with main

--- big_fireball.gd
+++ big_fireball.gd
@@ -6,7 +6,7 @@ var screen_size
 var caster 
 
 var velocity = Vector2(0.0, 0.0)
-var speed = 275
+var speed = 175
 var castFlag = false
 
 # Called when the node enters the scene tree for the first time.
